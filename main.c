ec7a942da264f51e29bb8a896fd0a42cee19481f
Pascal Ojukwu <luvpascal.ojukwu@yahoo.com>
2025-05-28 19:20:49 +0100

    Create main.c

---
+++ main.c
@@ -0,0 +1,14 @@
+int main() {
+
+int number1;
+int number2;
+int number3 = 2000;
+
+number1 = 450;
+number2 = 1000;
+int result = number1 + number2 + number3;
+
+printf("The answer is = %d", result);
+
+  return 0;
+}
